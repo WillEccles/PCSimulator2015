c5d81df7673251ffd148882a0073b706b483cdb1
Will Eccles <willeccles860+github@gmail.com>
2015-12-24 14:07:38 -0500

    Added hardwareinfo.h

---
+++ PCSimulator2015/hardwareinfo.h
@@ -0,0 +1,7 @@
+// this header contains all system information so that it may be referenced from other places
+#pragma once
+#define MOBO "Asus Maximus XXX"
+#define CPU "Intel Core i9-1738K"
+#define CPU_CLOCK 4.20
+#define RAM "430080MB DDR0-ECC"
+#define RAM_CLOCK 1866
---
+++ PCSimulator2015/motherboard.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+
+
+int main()
+{
+	std::cout << "Booting ";
+	return 0;
+}
+
