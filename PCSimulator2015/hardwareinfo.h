7c54dbc68d1b345ed51f264a7ff60a771415715f
Will Eccles <willeccles860+github@gmail.com>
2016-01-03 15:10:56 -0500

    Worked on motherboard.cpp and hardwareinfo.h

--- PCSimulator2015/hardwareinfo.h
+++ PCSimulator2015/hardwareinfo.h
@@ -5,3 +5,7 @@
 #define CPU_CLOCK 4.20
 #define RAM "430080MB DDR0-ECC"
 #define RAM_CLOCK 1866
+#define GPU "Nvidia Geforce GTX 420"
+#define GPU_CLOCK 1232
+#define VRAM 1024
+#define OS_VERSION "PC-SIM-OS v0.1"
--- PCSimulator2015/motherboard.cpp
+++ PCSimulator2015/motherboard.cpp
@@ -1,9 +1,18 @@
 #include "stdafx.h"
 
+void initInfo() {
+	std::cout << "Booting " << OS_VERSION << "..." << std::endl;
+	std::cout << std::endl << "HARDWARE INFO:" << std::endl;
+	std::cout << "    " << CPU << " @ " << CPU_CLOCK << "GHz" << std::endl;
+	std::cout << "    " << RAM << " @ " << RAM_CLOCK << "MHz" << std::endl;
+	std::cout << "    " << GPU << " @ " << GPU_CLOCK << "MHz" << std::endl;
+	std::cout << "    " << CPU << " @ " << CPU_CLOCK << "GHz, " << VRAM << "MB" << std::endl;
+}
 
 int main()
 {
-	std::cout << "Booting ";
+	initInfo();
+	std::cin.get();
 	return 0;
 }
 
